38a08885450adcbd33d9d39193ab6de569036f29
Daniel Samuel <dansam5000@gmail.com>
2021-10-22 13:00:39 +0100

    feat: Creat has-many side of person & belongs-to side of rental

--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -1,7 +1,7 @@
 require './corrector.rb'
 
 class Person
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rentals
   attr_reader :id
 
   def initialize(age, name = "Unknown", parent_permission = true)
@@ -10,6 +10,11 @@ class Person
     @age = age
     @name = name
     @parent_permission = parent_permission
+    @rentals = []
+  end
+
+  def add_rental(book, date)
+    Rental.new(date, book, self)
   end
 
   def can_use_services?
