47232e6925d0b319298fe2c5f917577c27656a92
Uzair Ali <uzairali19@gmail.com>
2021-10-25 05:00:05 +0500

    update logic in main.rb

--- main.rb
+++ main.rb
@@ -18,7 +18,39 @@ class App
     @rentals = rentals
   end
 
+  def logic
+    temp = true
+    while temp
+      puts 'Choose a number:'
+      puts '1 - List all books'
+      puts '2 - List all people'
+      puts '3 - Create a person'
+      puts '4 - Create a book'
+      puts '5 - Create a rental'
+      puts '6 - List all rentals for a given person id'
+      puts '0 - Exit'
 
+      num = gets.chomp
+
+      case num
+      when '1'
+        @books.list_all_books
+      when '2'
+        @people.list_people
+      when '3'
+        @people.create_person
+      when '4'
+        @books.create_book
+      when '5'
+        @rentals.create_rental(@books.books,@people.people)
+      when '6'
+        @rentals.list_all_rentals
+      when '0'
+        temp = false
+        p 'exiting'
+      end
+    end
+  end
 end
 
 def main
